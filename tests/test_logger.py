91c82f7fd8478a931401970d80c07d649172b3df
harshal_kusalkar <h41497254@gmail.com>
2026-06-26 19:43:15 +0530

    added tests dir to test scripts and functions

---
+++ tests/test_logger.py
@@ -0,0 +1,7 @@
+from src.utils.logger import get_logger
+
+logger = get_logger(__name__)
+
+logger.info("Testing logger working")
+logger.debug("Hi testing logger")
+logger.error("Logger is working.")
